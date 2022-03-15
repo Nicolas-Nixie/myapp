208efe91bbf2a9a3d24bbfc21daaf1c34fc2f177
unknown <mkleinnicolas@gmail.com>
2022-03-15 16:00:33 +0100

    ajout des fichiers

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Banner from './component/Banner';
+import Cart from './component/Cart';
+
+function App() {
+  return (
+    <div className="Body">
+      <header className="header">
+        <Banner/>
+        <Cart/>
+      </header>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Banner.jsx
@@ -0,0 +1,12 @@
+import styles from "./Banner.module.css"
+
+function Banner() {
+    //const title = "La maison Jungle";
+    return (
+    <div className={styles.lmjbanner}>
+        <h1>La maison Jungle</h1>
+    </div>
+    ) //{ title.toUpperCase }
+}
+
+export default Banner;
